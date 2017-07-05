2727611a06f4dd8e16887b7d1c64a0e51db1971c
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2017-07-05 13:15:35 +0300

    Refactoring

---
+++ Youtube DJ Bot/Youtube DJ Bot/TelegramClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputMessageContents;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Youtube_DJ_Bot
+{
+	class TelegramClient
+	{
+		private readonly TelegramBotClient _bot = new TelegramBotClient("413647505:AAFyR0CDTYzlnuGYt2soqdi5JXKCBSFvG50");
+		private User _me = null;
+
+		public delegate void TelegramMessagehandler(Message msg);
+		public event TelegramMessagehandler MessageReceived;
+
+		public TelegramClient()
+		{
+			Init();
+		}
+
+		public void Start()
+		{
+			_bot.StartReceiving();
+		}
+
+		public void Stop()
+		{
+			_bot.StopReceiving();
+		}
+
+		public void Init()
+		{
+			_bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+			_bot.OnMessage += BotOnMessageReceived;
+			_bot.OnMessageEdited += BotOnMessageReceived;
+			_bot.OnInlineQuery += BotOnInlineQueryReceived;
+			_bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+			_bot.OnReceiveError += BotOnReceiveError;
+
+			_me = _bot.GetMeAsync().Result;
+		}
+
+		public User Me
+		{
+			get { return _me; }
+		}
+
+		public async void SendReply(string text, long chatId)
+		{
+			if (string.IsNullOrEmpty(text))
+				return;
+
+			//await _bot.SendChatActionAsync(chatId, ChatAction.Typing);
+			await _bot.SendTextMessageAsync(chatId, text, replyMarkup: new ReplyKeyboardHide());
+		}
+
+		private void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+		{
+			Console.WriteLine("BotOnReceiveError: " + receiveErrorEventArgs.ApiRequestException.Message);
+			Debugger.Break();
+		}
+
+		private void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+		{
+			Console.WriteLine($"Received choosen inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+		}
+
+		private async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+		{
+			InlineQueryResult[] results = {
+				new InlineQueryResultVideo
+				{
+
+				}
+			};
+
+			await _bot.AnswerInlineQueryAsync(inlineQueryEventArgs.InlineQuery.Id, results, isPersonal: true, cacheTime: 0);
+		}
+
+		private async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+		{
+			var message = messageEventArgs.Message;
+
+			if (message == null || message.Type != MessageType.TextMessage) return;
+
+			if (MessageReceived != null)
+				MessageReceived(message);
+		}
+
+		private async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+		{
+			await _bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
+				$"Received {callbackQueryEventArgs.CallbackQuery.Data}");
+		}
+	}
+}
---
+++ Youtube DJ Bot/Youtube DJ Bot/YouTubeClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Youtube_DJ_Bot
+{
+	class YouTubeClient
+	{
+		public void GetFavoriteVideos()
+		{
+			FetchFavorites().Wait();
+		}
+
+		public List<string> Favorites
+		{
+			get { return _favoriteVideoUrls; }
+		}
+
+		private List<string> _favoriteVideoUrls = new List<string>();
+
+		private async Task FetchFavorites()
+		{
+			UserCredential credential;
+			var credentialsPath = Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase), @"..\..\..\..\client_secret.json").Replace("file:\\", "");
+			using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
+			{
+				credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					// This OAuth 2.0 access scope allows for read-only access to the authenticated 
+					// user's account, but not other types of account access.
+					new[] { YouTubeService.Scope.YoutubeReadonly },
+					"user",
+					CancellationToken.None,
+					new FileDataStore(this.GetType().ToString())
+				);
+			}
+
+			var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = this.GetType().ToString()
+			});
+
+			var channelsListRequest = youtubeService.Channels.List("contentDetails");
+			channelsListRequest.Mine = true;
+			var channels = await channelsListRequest.ExecuteAsync();
+
+			if (channels.Items.Count <= 0)
+			{
+				Console.Write("ERROR: no YT channels found.");
+				return;
+			}
+
+			var favoritesListId = channels.Items[0].ContentDetails.RelatedPlaylists.Favorites;
+
+			string nextPageToken = "";
+			var favoriteVideos = new List<PlaylistItem>();
+			while (nextPageToken != null)
+			{
+				var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
+				playlistItemsListRequest.PlaylistId = favoritesListId;
+				playlistItemsListRequest.MaxResults = 50;
+				playlistItemsListRequest.PageToken = nextPageToken;
+
+				var playlistItemsListResponse = await playlistItemsListRequest.ExecuteAsync();
+
+				favoriteVideos.AddRange(playlistItemsListResponse.Items);
+				nextPageToken = playlistItemsListResponse.NextPageToken;
+			}
+
+			for (int i = 0; i < favoriteVideos.Count; ++i)
+			{
+				var item = favoriteVideos[i];
+				Console.WriteLine((i + 1).ToString() + ": " + item.Snippet.Title);
+				if (item.Snippet.Title != "Deleted video" && item.Snippet.Title != "Private video")
+					_favoriteVideoUrls.Add("https://youtu.be/" + item.Snippet.ResourceId.VideoId);
+			}
+		}
+	}
+}
